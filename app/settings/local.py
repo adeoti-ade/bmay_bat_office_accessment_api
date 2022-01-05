b33a86a56918dcc467a0799a678fb926b1751d8c
Adeoti Adegboyega <adeotiadegboyega@gmail.com>
2022-01-05 08:15:36 +0100

    initial commit

---
+++ app/settings/local.py
@@ -0,0 +1,12 @@
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
